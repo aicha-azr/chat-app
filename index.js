e4daa8e0396cf8a2c94c4e44057e862859102861
aicha-azr <aichaazeroual03@gmail.com>
2024-07-24 23:05:02 +0100

    updates

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const io = require('socket.io')(3000, {
-    cors: {
-      origin: '*', // Allows all origins. You can restrict this to specific origins if needed.
-      methods: ['GET', 'POST']
-    }
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
+});
+const users = {};
+
+io.on('connection', socket => {
+  console.log('new user');
+
+  socket.on('send-chat-message', message => {
+    console.log(message);
+    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] });
   });
-  const users = {}
-  io.on('connection', socket => {
-    console.log('new user');
-    socket.emit('chat-message', 'hello world!');
-    socket.on('send-chat-message', message =>{
-        console.log(message)
-        socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
-    })
-    socket.on('new-user', name =>{
-        users[socket.id] = name
-        socket.broadcast.emit('user-connecton', message)
-    })
+
+  socket.on('new-user', name => {
+    users[socket.id] = name;
+    socket.broadcast.emit('user-connection', name); // Correction ici
   });
-  
+});
--- script.js
+++ script.js
@@ -2,24 +2,25 @@ const socket = io('http://localhost:3000');
 const messageContainer = document.getElementById('message-container');
 const messageForm = document.getElementById('send-container');
 const messageInput = document.getElementById('message-input');
-const name = prompt('what is your name?')
+const name = prompt('What is your name?')
 appendMessage('You joined');
 socket.emit('new-user', name);
 
-socket.on('chat-message', data =>{
+socket.on('chat-message', data => {
     appendMessage(`${data.name}: ${data.message}`);
-})
-socket.on('user-connection', name =>{
+});
+socket.on('user-connection', name => {
     appendMessage(`${name} connected`);
-})
-messageForm.addEventListener('submit', e =>{
+});
+messageForm.addEventListener('submit', e => {
     e.preventDefault();
     const message = messageInput.value;
-    socket.emit('send-chat-message', message)
-    messageInput.value = ''
-})
+    socket.emit('send-chat-message', message);
+    messageInput.value = '';
+});
+
 function appendMessage(message) {
-    const messageElement = document.createElement('div')
-    messageElement.innerText = message
-    messageContainer.append(messageElement)
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageContainer.append(messageElement);
 }
